065dca632de3ddcb04cd4941535354ea8d28b542
HeSer <B@nd@r1359>
2022-08-06 15:02:19 -0400

    first commit, August6th, figureing out ml in pycharm

---
+++ workwithexcel.py
@@ -0,0 +1,15 @@
+# print("Hello World!")
+# libraries needed
+# pandas, mathplotlib, scikit-learn, numpy
+
+import pandas as pd
+
+df = pd.read_csv("C:\\Users\\HeSer\\PycharmProjects\\pythonProject\\AI_self_learn\\vgsales.csv")
+#  to prevent the unicode error, use double back slass
+
+# print(vg)
+# print(df.shape)
+# print(df.describe())
+print(df.values)
+
+
